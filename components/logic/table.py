ac6aa5a20da776bded5eddebe48576644bf09ba2
antonSuprun <winetu@ya.ru>
2012-12-10 22:22:41 +0300

    small refactoring

--- components/logic/table.py
+++ components/logic/table.py
@@ -3,47 +3,46 @@ class table:
     def __init__(self,information):
         self.__information=information
     def makeTextTable(self):
-        table=self.genereteDisk()
-        maxSize=0
+        table = self.genereteDisk()
+        maxSize = 0
         for var in table['var']:
-            if maxSize<len(var):
-                maxSize=len(var)
-        result=self.getOneLine(table['var'],maxSize)+"\n"
-        line=self.getHorizontalLine(len(result))+"\n"
-        result=line+result+line
+            if maxSize < len(var):
+                maxSize = len(var)
+        result = self.getOneLine(table['var'],maxSize) + "\n"
+        line = self.getHorizontalLine(len(result)) + "\n"
+        result = line + result+line
         for variant in table['variants']:
-            line=self.getOneLine(variant,maxSize);
-            result=result+line+"\n"
-            result=result+self.getHorizontalLine(len(line))+"\n"
+            line = self.getOneLine(variant,maxSize);
+            result = result + line + "\n"
+            result = result + self.getHorizontalLine(len(line)) + "\n"
         return result
     def getHorizontalLine(self,length):
         result=""
         for i in range(length):
-            result=result+"_"
+            result = result + "_"
         return result
     def getOneLine(self,list,maxSize):
-        result=""
+        result = ""
         for var in list:
-            result=result+"|"
-            center=maxSize/2+1;
-            tab=center-len(var)/2
+            result = result + "|"
+            center = maxSize/2 + 1;
+            tab = center - len(var)/2
             for i in range(tab):
-                result=result+" "
-            result=result+var
+                result = result + " "
+            result = result + var
             for i in range(tab):
-                result=result+" "
-        return result+"|"
+                result = result + " "
+        return result + "|"
 
     def genereteDisk(self):
-        disc = {'var': ['x', 'y','z', "x&y", "x&y|z"]}
-                            # x  y   z  x&y x&y|z
-        disc['variants'] = [["0","0","0","0","0"],
-                            ["0","0","1","0","1"],
-                            ["0","1","0","0","0"],
-                            ["0","1","1","0","1"],
-                            ["1","0","0","0","0"],
-                            ["1","0","1","0","1"],
-                            ["1","1","0","1","1"],
-                            ["1","1","1","1","1"]
+        disc = {'var': ['x', 'y', 'z', "x&y", "x&y|z"]}
+        disc['variants'] = [["0", "0", "0", "0", "0"],
+                            ["0", "0", "1", "0", "1"],
+                            ["0", "1", "0", "0", "0"],
+                            ["0", "1", "1", "0", "1"],
+                            ["1", "0", "0", "0", "0"],
+                            ["1", "0", "1", "0", "1"],
+                            ["1", "1", "0", "1", "1"],
+                            ["1", "1", "1", "1", "1"]
         ]
         return disc
